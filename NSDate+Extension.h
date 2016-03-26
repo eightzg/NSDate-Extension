36680192a9618d258785513d3ef5a72dd0f2a8b7
eightzg <apple@appledeMacBook-Pro-3.local>
2016-03-26 10:05:41 +0800

    NSDate分类
    判断当前的日期

---
+++ NSDate+Extension.h
@@ -0,0 +1,23 @@
+//
+//  NSDate+Extension.h
+//
+//  Created by eigthzg on 16-03-26.
+//  Copyright (c) 2016年 eightzg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Extension)
+/**
+ *  判断某个时间是否为今年
+ */
+- (BOOL)isThisYear;
+/**
+ *  判断某个时间是否为昨天
+ */
+- (BOOL)isYesterday;
+/**
+ *  判断某个时间是否为今天
+ */
+- (BOOL)isToday;
+@end
